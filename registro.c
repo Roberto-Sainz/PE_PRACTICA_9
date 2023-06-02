201ed9e649b29cd950e8690e6c21fe7bcaf08c4f
Roberto-Sainz <124394461+Roberto-Sainz@users.noreply.github.com>
2023-06-02 10:39:41 -0700

    Create registro.c

---
+++ registro.c
@@ -0,0 +1,9 @@
+#include "registro.h"
+#include <stdio.h>
+
+struct alumno leerRegistro(FILE *fp, long numeroDeRegistro) {
+    struct alumno reg;
+    fseek(fp, numeroDeRegistro * sizeof(struct alumno), SEEK_SET);
+    fread(&reg, sizeof(struct alumno), 1, fp);
+    return reg;
+}
